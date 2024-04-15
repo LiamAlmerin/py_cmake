8d1cd7846cd34e0874dde46c5f079b2459847dde
liamalmerin <liamalmerin@gmail.com>
2024-04-15 14:33:37 -0400

    added .gitignore ability

--- README.md
+++ README.md
@@ -6,20 +6,29 @@ Python scripts to aid in making C++ CMake projects.
 
 ### New Project
 ```
-pymake.py -p <project_name> 
+pymake.py -p -n=<project_name> 
 ```
 creates new project with name <project_name>.
+### Add .gitignore
+```
+pymake.py -i -n=<directory_path_name> 
+```
+Adds .gitignore in <directory_path_name>. One can optionally add the -i, when making a new project.
+```
+pymake.py -i
+```
+Adds .gitignore in current directory.
 ### New Directory
 ```
-pymake.py -p <directory_path_name> 
+pymake.py -p -n=<directory_path_name> 
 ```
 creates new directory at src/<directory_path_name>.
 ### New File
 ```
-pymake.py -hpp <path_to_file/file_name> 
+pymake.py -hpp -n=<path_to_file/file_name> 
 ```
 creates header source file including header guard with extension the hpp. 
 ```
-pymake.py -cpp <path_to_file/file_name> 
+pymake.py -cpp -n=<path_to_file/file_name> 
 ```
 creates C++ source file with extension cpp and adds it to the appopriate CMakeLists.txt file for compilation. 
--- pymake.py
+++ pymake.py
@@ -5,8 +5,6 @@ import scripts.new_src_dir as nsd
 import os
 import argparse
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-
 parser = argparse.ArgumentParser()
 parser.add_argument("-hpp","--header", help="Creates hpp file with header guard",
                     action="store_true")
@@ -16,10 +14,12 @@ parser.add_argument("-p","--new_project",  help="Creates new project",
                     action="store_true")
 parser.add_argument("-d","--new_src_directory",  help="Creates a new directory under src along with CMakeLists.txt file", 
                     action="store_true")
-parser.add_argument("name")
-
+parser.add_argument("-i","--ignore",  help="Adds .gitignore file to directory", 
+                    action="store_true")
+parser.add_argument("-n","--name", required=False)
 args = parser.parse_args()
 
+script_dir = os.path.dirname(os.path.abspath(__file__))
 current_dir = os.getcwd()
 
 if args.new_project:
@@ -30,3 +30,12 @@ if args.header:
     nsf.newHeader(current_dir, args.name)
 if args.cpp_file:
     nsf.newCpp(current_dir, args.name)
+if args.ignore:
+    with  open(script_dir + '/data/.gitignore') as f:
+        ignore_filedata = f.read()
+    if args.name:
+        with open(args.name + '/.gitignore', 'w') as f:
+            f.write(ignore_filedata)
+    else:
+        with open('.gitignore', 'w') as f:
+            f.write(ignore_filedata)
